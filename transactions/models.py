5900384ae25707530149952d94dcafe24dd4a772
Ismail <142449970+Dop1o@users.noreply.github.com>
2026-05-11 18:21:13 +0400

    Update models.py

--- transactions/models.py
+++ transactions/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -65,7 +64,7 @@ class Transaction(models.Model):
         validators=[MinValueValidator(0.01)]
     )
     description = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPE, default='expense')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     tags = models.ManyToManyField(Tag, blank=True)
